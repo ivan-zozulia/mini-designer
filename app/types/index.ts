c08a2530b20041fb2d8e5f844feabcdb06e8783f
Ivan Zozulia <ivan.p.zozulya@gmail.com>
2026-03-15 17:39:34 +0100

    feat: add types

---
+++ app/types/index.ts
@@ -0,0 +1,24 @@
+export interface Color {
+  name: string;
+  color: string;
+  price: number;
+}
+
+export interface Motive {
+  name: string;
+  img: string;
+  price: number;
+}
+
+export interface CheckoutForm {
+  name: string;
+  address: string;
+}
+
+export type OrderErrors = Partial<Record<keyof CheckoutForm, string[]>>;
+
+export interface OrderErrorResponse {
+  message: string;
+  statusText: string;
+  errors: OrderErrors;
+}
