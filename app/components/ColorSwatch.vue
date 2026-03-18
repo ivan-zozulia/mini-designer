9cef948677bfa93573adc53b006331b0e13e9855
Ivan Zozulia <ivan.p.zozulya@gmail.com>
2026-03-18 13:14:28 +0100

    refactor: extract ColorSwatch component

---
+++ app/components/ColorSwatch.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+  color: string
+}>()
+</script>
+
+<template>
+  <svg
+    viewBox="0 0 36 36"
+    class="size-9"
+    :aria-label="name"
+  >
+    <g transform="translate(18 18)">
+      <circle
+        r="12"
+        :fill="color"
+        class="stroke-neutral-500"
+        stroke-width="1"
+      />
+      <circle
+        r="16"
+        fill="none"
+        class="group-data-selected:stroke-blue-700"
+        stroke-width="2"
+      />
+    </g>
+  </svg>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -43,26 +43,10 @@ const visibleCount = computed(() => isCompact.value ? 3 : 4)
           :visible-count="visibleCount"
         >
           <template #default="{ item }">
-            <svg
-              viewBox="0 0 36 36"
-              class="size-9"
-              :aria-label="item.name"
-            >
-              <g transform="translate(18 18)">
-                <circle
-                  r="12"
-                  :fill="item.color"
-                  class="stroke-neutral-500"
-                  stroke-width="1"
-                />
-                <circle
-                  r="16"
-                  fill="none"
-                  class="group-data-selected:stroke-blue-700"
-                  stroke-width="2"
-                />
-              </g>
-            </svg>
+            <ColorSwatch
+              :name="item.name"
+              :color="item.color"
+            />
           </template>
         </DesignListbox>
       </div>
